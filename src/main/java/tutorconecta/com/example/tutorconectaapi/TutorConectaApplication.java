76ffa974e207db86003bf516b3eaa7009e31cbd8
Franco <carrionq.franco28@gmail.com>
2026-09-21 09:17:34 -0500

    chore: inicialización de la estructura base Spring Boot

---
+++ src/main/java/tutorconecta/com/example/tutorconectaapi/TutorConectaApplication.java
@@ -0,0 +1,13 @@
+package tutorconecta.com.example.tutorconectaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorConectaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorConectaApplication.class, args);
+    }
+
+}
