5a06684929ec859d31a1dd546ee539bada199813
Franco Estefano Carrion <carrionq.franco28@gmail.com>
2026-09-21 19:37:33 -0500

    Revert "Agregando CRUD completo de Tablas: "Calificacion" y "Pago""

--- src/main/java/tutorconecta/com/example/tutorconectaapi/repositories/PagoRepository.java
+++
@@ -1,7 +0,0 @@
-package tutorconecta.com.example.tutorconectaapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tutorconecta.com.example.tutorconectaapi.entities.Pago;
-
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-}
