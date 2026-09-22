e219bdb807dd32c8d11052119cc30cc7a0e0dbc9
Franco Estefano Carrion <carrionq.franco28@gmail.com>
2026-09-21 21:26:23 -0500

    Revert "Revert "feat: agregar CRUD de Materias y Sesiones""

---
+++ src/main/java/tutorconecta/com/example/tutorconectaapi/dtos/MateriaRequestDTO.java
@@ -0,0 +1,15 @@
+package tutorconecta.com.example.tutorconectaapi.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class MateriaRequestDTO {
+    @NotBlank
+    private String nombre;
+
+    private String descripcion;
+
+    @NotBlank
+    private String nivelEducativo;
+}
